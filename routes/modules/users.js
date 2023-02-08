11ea23743b12b9b8c106b763d79ed4a8733858cd
ellie32411 <ellie32411@gmail.com>
2023-02-08 19:44:53 +0800

    feat: add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('../../models/users')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
@@ -13,4 +14,27 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('註冊過了')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    }
+  })
+    .catch(error => console.log(error))
+})
 module.exports = router
