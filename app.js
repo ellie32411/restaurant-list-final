e865020bbfd8b371709d6a64f3adfd7296d0ad31
ellie32411 <ellie32411@gmail.com>
2023-02-08 19:20:49 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 const methodOverride = require("method-override")
-const mongoose = require("mongoose")
 const Restaurant = require("./models/Restaurant")
-const router = require("./routes")
-
-mongoose.connect("mongodb+srv://ellie32411:jessica188@cluster0.z2detec.mongodb.net/restaurant-list-final?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const db = mongoose.connection
-
-db.on("error", () => {
-  console.log("mongodb error!")
-})
-
-db.once("open", () => {
-  console.log("mongodb connected!")
-})
+const routes = require("./routes")
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -28,7 +13,7 @@ app.set("view engine", "handlebars")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
-app.use(router)
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
--- models/seeder/restaurantSeeder.js
+++ models/seeder/restaurantSeeder.js
@@ -1,17 +1,10 @@
-const mongoose = require("mongoose")
-const Restaurant = require("../restaurant")
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+} 
+const db = require('../../config/mongoose')
+const Restaurant = require("../Restaurant")
 const restaurantList = require("../../restaurant.json").results
 
-mongoose.connect("mongodb+srv://ellie32411:jessica188@cluster0.z2detec.mongodb.net/restaurant-list-final?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const db = mongoose.connection
-
-db.on("error", () => {
-  console.log("mongodb error!")
-})
 
 db.once("open", () => {
   console.log("running restaurantSeeder script...")
